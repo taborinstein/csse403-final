97fd3cb18407e417e44ef7aee6005b2660f042a1
rhit-zhouck <148365637+rhit-zhouck@users.noreply.github.com>
2026-05-21 02:40:55 -0400

    Created new level_5.nim

---
+++ src/level_data/level_5.nim
@@ -0,0 +1,12 @@
+import maze_creation
+
+let level_5* = define_maze:
+    w w w w w w w w w w w w w w w w
+    w - - - K - w w w - - - - w - w
+    w w w w - w w X w - w - - w - w
+    w - - - - w w D w w w w - - - w
+    w - w w - w w S - - 1 w w - - w
+    w - w w - w - - w w w - - - - w
+    w - w w - w w - w - w w w w - w
+    w - - - - - - - - - - - - - - w
+    w w w w w w w w w w w w w w w w 
