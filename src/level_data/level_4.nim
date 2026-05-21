e8ff8dcad446d79e10468c5881f8ca797f6c1ed0
rhit-zhouck <148365637+rhit-zhouck@users.noreply.github.com>
2026-05-21 02:22:10 -0400

    changed 2 keys to enemies (doesn't change the level, just foreshadowing)

--- src/level_data/level_4.nim
+++ src/level_data/level_4.nim
@@ -5,7 +5,7 @@ let level_4* = define_maze:
     w - - - - - - - - - - - - - - - - - - - - - - - - - - - - w
     w - w - - w - w w w w - w - - - - w - - - - w w w w - w - w
     w - w - - w - w - - - - w - - - - w - - - - w K K w - w - w
-    w - w w w w - w w w - - w - - - - w - - - - w K K w - w - w
+    w - w w w w - w w w - - w - - - - w - - - - w 1 1 w - w - w
     w - w - - w - w - - - - w - - - - w - - - - w K K w - - - w
     w - w - - w - w w w w - w w w w - w w w w - w w w w - X - w
     w S - - - - - - - - - - - - - - - - - - - - - - - - - - - w
