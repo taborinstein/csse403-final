f894a49b4e77d87204a9bdf77b1ffadfef079e24
rhit-zhouck <148365637+rhit-zhouck@users.noreply.github.com>
2026-05-21 02:31:45 -0400

    Create level_5.nim

---
+++ src/level_data/level_5.nim
@@ -0,0 +1,20 @@
+import maze_creation
+
+let level_5* = define_maze:
+    w w w w w w w w w w w w w w w w w w w w w w w w w w w w w w
+    w w w w w w w w w - - - - - w w w w w w w w w w w w w w w w
+    w w w w - w - - - - w w w - - w - w - - - - - - - - - - - w
+    w X w w 1 w w w w - w - w - - - - w - w - - w w w w - w - w
+    w D w w - w 1 - - - w - w - w w - - - w - - w - - w - w - w
+    w D w w - w w w w w w - w - w w - w - w - - w w w w w w w w
+    w - w w - w w w - - - - w - - - - w - - - - w - - w - - - w
+    w - w w - w w w - w w w w w - w - w w - w - w w w w - K - w
+    w S - - - - - - - - - - - - - - - - - - - - - - - - - - - w
+    w w w w - w w w w w w w w w w w w w w w w w w w w w w w w w
+    w w w w - w w - - - - w w w - - - - - - - - - - w w w w - w
+    w w w w - w w w - - - - - - - w w w w w w w w - - - - - - w
+    w w w w - w w w w w w w w w - - - w w - - - - - w w w w - w
+    w w w w - - - - - - w w w - w w - w w w - w w w w w w w - w
+    w w w w w w w w w - - - - - - - - - - - - w w w w - - - - w
+    w w w w w w w w w w w w w w w w w w w w w w - - - - w w K w
+    w w w w w w w w w w w w w w w w w w w w w w w w w w w w w w
