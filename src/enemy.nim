b00e3fe669c23b0f1d850cd19211ed1fa46314d8
zhouck <zhouck@rose-hulman.edu>
2026-05-21 23:11:12 -0400

    up/down and left/right enemies both work now

--- src/enemy.nim
+++ src/enemy.nim
@@ -19,48 +19,71 @@ type
         time_since_action: float
         updateSelf*: proc (enemy: Enemy, elapsed: float)
 
-proc init*(enemy: Enemy, speed: float, graphicName: string, radius: int) = 
+proc init*(enemy: Enemy, speed: float) = 
     enemy.initEntity()
     enemy.speed = speed
-    enemy.graphic = gfxData[graphicName]
     
     enemy.tags.add("enemy")
     enemy.physics = platformerPhysics
 
-# stationary enemy... 
-proc newEnemy1(): Enemy =
-    var enemy = new Enemy
-    init enemy, 0, "enemy1", 32
+proc addFlashingKeySprite(enemy: Enemy) =
+    enemy.radius = 32
+    enemy.graphic = gfxData["enemy1"]
     enemy.initSprite((30,30))
     discard enemy.addAnimation("flash", [0,1], 1/2)
     enemy.play("flash", -1)
     enemy.centrify()
     enemy.collider = enemy.newCircleCollider((0.0,0.0), enemy.radius) 
+
+# stationary enemy... 
+proc newEnemy1(): Enemy =
+    var enemy = new Enemy
+    init enemy, 0 
+    enemy.addFlashingKeySprite()
+    enemy.updateSelf = proc(enemy: Enemy, elapsed:float) = discard
     return enemy
 
 # left right
 proc newEnemy2(): Enemy = 
     var enemy = new Enemy
-    init enemy, 750, "enemy1", 32
-    enemy.initSprite((30,30))
-    enemy.centrify()
-    enemy.collider = enemy.newCircleCollider((0.0,0.0), enemy.radius)
+    init enemy, 1024
+    enemy.addFlashingKeySprite()
     enemy.vel.x = -enemy.speed
+    enemy.updateSelf = proc(enemy: Enemy, elapsed:float) = 
+        enemy.time_since_action += elapsed
+        if enemy.time_since_action >= 1:
+            enemy.time_since_action = 0
+            enemy.vel.x = -enemy.vel.x
     return enemy
+
+# up down
+proc newEnemy3(): Enemy = 
+    var enemy = new Enemy
+    init enemy, 1024
+    enemy.addFlashingKeySprite()
+    enemy.vel.y = -enemy.speed
+    enemy.updateSelf = proc(enemy: Enemy, elapsed:float) = 
+        enemy.time_since_action += elapsed
+        if enemy.time_since_action >= 1:
+            enemy.time_since_action = 0
+            enemy.vel.y = -enemy.vel.y
+    return enemy
+
+
+proc newEnemy4(): Enemy =
+    return newEnemy3()
 
 proc newEnemy*(enemyType: int): Enemy =
     var enemy = new Enemy
     case enemyType:
         of 1:
             enemy = newEnemy1()
-            enemy.updateSelf = proc(enemy: Enemy, elapsed:float) = discard
         of 2: 
             enemy = newEnemy2()
-            enemy.updateSelf = proc(enemy: Enemy, elapsed:float) = 
-                enemy.time_since_action += elapsed
-                if enemy.time_since_action >= 2:
-                    enemy.time_since_action = 0
-                    enemy.vel.x = -enemy.vel.x
+        of 3:
+            enemy = newEnemy3()
+        of 4:
+            enemy = newEnemy4()
         else:
             echo "unrecognized enemy"
             return newEnemy1()
