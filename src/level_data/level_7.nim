fa04b76fa1dfa45d9646d55ec69fd02b3d80c017
rhit-zhouck <148365637+rhit-zhouck@users.noreply.github.com>
2026-05-22 00:16:14 -0400

    Create level_7.nim

---
+++ src/level_data/level_7.nim
@@ -0,0 +1,23 @@
+import maze_creation
+
+let level_7* = define_maze:
+    w w w w w w w w w w w w w w w w w w w w w w w w w w w w w w
+    w X - - w w w w w w w w w w w w - - - - - - - - - - - - 2 w
+    w w w D w w w - - - - - - w w w - w w w - - - - - - - - 2 w
+    w w w - - w w - - - - - - w w w K w w w - - - - - - - - 2 w
+    w w w w - w w - - - - - - - w w w w w w w w w w w w w w - w
+    w - - - - - - - - - - - - - - w - w - - - - - - - - - - - w
+    w - w w - w w - - - - - - - - - - w - w - - w w w w - w - w
+    w - w w w w w - - - - - - - w w - - - w - - w w w w - w - w
+    w - w w - w w - - - - - - - w w - w w w w - w w w w w w w w
+    w - w w - w w 3 3 3 3 3 3 - - - - w w - w - w - w w - - - w
+    w - w w - w w w w w w w w w w D w w w - w w w - w w - K - w
+    w 1 - w - w - - - - - - - - - 2 - - - - - - - - - - - - 2 w
+    w w w w - w w w w w w w w w w - w w w w w w w w w w w w w w
+    w w w w - w w - - - - w w w w - w - - - - - - - - 2 w w - w
+    w w w w - w w w - - - - - w w D w w w w w w w w w w w w - w
+    w w w w - w w w w w w w - - - - - - - - - - w w w w w w - w
+    w K - - 3 - - - - - w w - - w - w w w w w w w w w w w w - w
+    w w w w w w w w w - - - - - w - w - - - - w w w w - - - - w
+    w w w w w w w w w w w w w w w S w w w w w w - - - - w w K w
+    w w w w w w w w w w w w w w w w w w w w w w w w w w w w w w
