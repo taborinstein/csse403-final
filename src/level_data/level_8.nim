3be04bce0920840d19b71ca4b75a726859badd92
rhit-zhouck <148365637+rhit-zhouck@users.noreply.github.com>
2026-05-22 00:31:25 -0400

    Create level_8.nim

---
+++ src/level_data/level_8.nim
@@ -0,0 +1,31 @@
+import maze_creation
+
+let level_8* = define_maze:
+    w w w w w w w w w w w w w w w w w w w w
+    w - - w w w X 1 1 w w - - - 1 w - - - w
+    w - - w - w w w - - - - w w w w - w w w
+    w - w w - w w w w w w - w - w w - w 1 w
+    w - w - - - - - - - - - - - - w - w - w
+    w - w w - w w - w w w w w w w w - w - w 
+    w - - - - - - - - 2 - - - - - - - - 2 w
+    w - - - - - - - - 2 - - - - - - - - 2 w
+    w - - - - - - - - 2 - - - - - - - - 2 w
+    w - - - - - - - - 2 - - - - - - - - 2 w
+    w - - - - - - - - 2 - - - - - - - - 2 w
+    w - - - - - - - - 2 - - - - - - - - 2 w
+    w - - - - - - - - 2 - - - - - - - - 2 w
+    w - - - - - - - - 2 - - - - - - - - 2 w
+    w 3 3 3 3 3 3 3 3 2 3 3 3 3 3 3 3 3 2 w 
+    w - - - - - - - - 2 - - - - - - - - 2 w
+    w - - - - - - - - 2 - - - - - - - - 2 w
+    w - - - - - - - - 2 - - - - - - - - 2 w
+    w - - - - - - - - 2 - - - - - - - - 2 w
+    w - - - - - - - - 2 - - - - - - - - 2 w
+    w - - - - - - - - 2 - - - - - - - - 2 w
+    w - - - - - - - - 2 - - - - - - - - 2 w
+    w - - - - - - - - 2 - - - - - - - - 2 w
+    w 3 3 3 3 3 3 3 3 2 3 3 3 3 3 3 3 3 2 w 
+    w w w w w w w w w - w w w w w w w w w w
+    w w w w w w w w w - w - - - - w w w w w
+    w w w w w w w w w S w w w w w w w w w w
+    w w w w w w w w w w w w w w w w w w w w 
